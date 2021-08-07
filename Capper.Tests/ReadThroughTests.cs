acc0702a71e2750d1d1817279bc87bcf6b46c09c
Roger Creyke <creyke@gmail.com>
2021-08-07 11:19:18 +0100

    Added dapper style syntax.

---
+++ Capper.Tests/ReadThroughTests.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Capper.Tests
+{
+    public class ReadThroughTests
+    {
+        private readonly IDistributedCache cache;
+
+        public ReadThroughTests()
+        {
+            cache = new MemoryDistributedCache(Options.Create(new MemoryDistributedCacheOptions()));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async Task CanRead(int key)
+        {
+            var response = await cache.ReadThroughAsync(key, () =>
+                Task.FromResult((IEnumerable<string>)(new[] { key.ToString() })));
+
+            Assert.Equal(key.ToString(), response.First());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async Task CanReadDapper(int key)
+        {
+            var connection = new SqlConnection();
+
+            var response = await cache.ReadThroughAsync(key, async () => 
+                await connection.QueryAsync<string>("SELECT * FROM c"));
+
+            Assert.Equal(key.ToString(), response.First());
+        }
+    }
+}
---
+++ Capper/IDistributedCacheExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Capper
+{
+    public static class IDistributedCacheExtensions
+    {
+        public static async Task<IEnumerable<T>> ReadThroughAsync<TKey, T>(this IDistributedCache cache, TKey key, Func<Task<IEnumerable<T>>> func)
+        {
+            return await func.Invoke();
+        }
+    }
+}
